13c2a655c6a657793c82bf901bcf5525ef63e1a0
joaocchugo <joao.gomes4@estudante.ifto.edu.br>
2023-12-05 11:20:15 -0300

    Alteração de 5 de Dezembro

--- src/main/java/com/example/demo/controller/ProdutoController.java
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produto") /** Caminho inicial dos métodos do Controller */
@@ -35,8 +37,33 @@ public class ProdutoController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping
-    public Produto insert(@RequestBody Produto produto){
+    @DeleteMapping("/{id}")
+    /** Deleta produto por id específico */
+    public void delete(@PathVariable long id){
+       /**Programação funcional*/
+        repository.findAndDelete(id);
+    }
+
+    @PutMapping("{id}")
+    /** Alterar produto por id específico */
+    public Produto update(@PathVariable long id, @RequestBody Produto produto){
+        final var msg = "O ID informado nao coincide com o ID do objeto passado";
+        if (id!= produto.getId()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "msg");
+        }
         return repository.save(produto);
     }
+
+    @PostMapping
+    public Produto insert(@RequestBody Produto produto){
+
+        return repository.save(produto);
+    }
+
+    /**
+     * Conjunto de documentações
+     * https://www.openapis.org/
+     * https://swagger.io/
+     * http://localhost:8080/swagger-ui/index.html
+     * */
 }
